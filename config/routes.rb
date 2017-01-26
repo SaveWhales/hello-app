a368ebfcc81187eba21f2d60c0f6d07952ea38e4
Saving Whalesharks <savethewhalesharks3@gmail.com>
2017-01-26 05:30:01 +0000

    Changed root action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  #root 'application#hello'
+  root 'application#hello'
   #root 'application#goodbye'
-  root 'application#esperanto'
+  #root 'application#esperanto'
 end
